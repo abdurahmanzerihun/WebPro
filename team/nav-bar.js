b3b9713d7d3078ed42ddd1a6630f28ded822d98b
abdi <abdurrahmanzerihun@gmail.com>
2025-06-19 06:51:14 -0400

    Imported unfinished team folder from team branch

---
+++ team/nav-bar.js
@@ -0,0 +1,23 @@
+function toggleMenu() {
+  const navLinks = document.getElementById("nav-links");
+  navLinks.classList.toggle("show");
+}
+
+  function openModal(id) {
+    document.getElementById(id).style.display = 'block';
+  }
+
+  function closeModal(id) {
+    document.getElementById(id).style.display = 'none';
+  }
+
+  // Optional: Close modal when clicking outside
+  window.onclick = function(event) {
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach(modal => {
+      if (event.target === modal) {
+        modal.style.display = 'none';
+      }
+    });
+  }
+
