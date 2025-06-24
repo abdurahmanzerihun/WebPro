d9d4cd9c4d1ab61ff62a8646a2ad2dc17131fe21
chillin9panda <duressamarkos9@gmail.com>
2025-06-24 16:48:08 +0300

    create default admin user

---
+++ admin/php/add_new_admin.php
@@ -0,0 +1,16 @@
+<?php
+require_once "../../php/connect.php";
+
+$email = "admin@novadev.com";
+$first_name = "John";
+$last_name = "Doe";
+$password = password_hash("novadevadmin", PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO admin (email, first_name, last_name, password) VALUES (?, ?, ?, ?)";
+$stmt = $connection->prepare($sql);
+
+if ($stmt->execute([$email, $first_name, $last_name, $password])) {
+  echo "Admin Account Created!";
+} else {
+  echo "Error: " . $stmt->error;
+}
